065edef5f2a9216ffa61dd5bd2a47d7dc1ac3d88
angelique <angeliquecorbisier@gmail.com>
2020-06-19 11:14:56 +0200

    updatelast

--- my-app/app/Http/Middleware/Authenticate.php
+++ my-app/app/Http/Middleware/Authenticate.php
@@ -12,10 +12,24 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    // protected function redirectTo($request)
+    // {
+    //     if (! $request->expectsJson()) {
+    //         return route('login');
+    //     }
+    // }
+
+    public function handle($request, Closure $next, $guard = null)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->guest('login');
+            }
         }
+
+        return $next($request);
     }
+
 }
